50c3b977ecea46fa17b9bb67ea74efb563cb7948
Delaney McLachlan <dmclachl@uwo.ca>
2023-11-07 21:33:26 -0500

    added backend for get requests

--- server/server.js
+++ server/server.js
@@ -19,6 +19,43 @@ app.get('/', (req, res) => {
 });
 
 
+//get all superhero info for a given superhero id
+app.get('/superhero/:id', (req, res) => {
+    const idNumber = parseInt(req.params.id);
+    const hero = superHeroInfoData.find(sh => sh.id === idNumber);
+    if (hero) {
+        res.json(hero);
+    } else {
+        res.status(404).send('Superhero not found given ID');
+    }
+});
+
+//get all powers for a given superhero ID
+app.get('/superhero/:id/powers', (req, res) => {
+    const id = parseInt(req.params.id);
+    const shPowers = superHerosPowerData.find(sh => sh.hero_names === superHeroInfoData[id].name);
+    if (shPowers) {
+        res.json(shPowers);
+    } else {
+        res.status(404).send('Invalid: Hero powers not found');
+    }
+});
+
+
+
+//get all available publisher names
+app.get('/publishers', (req, res) => {
+    const pblisher = [...new Set(superHeroInfoData.map(sh => sh.Publisher))];
+    res.json(pblisher);
+});
+
+//get names of heroes with powers
+app.get('/powers', (req, res) => {
+    const heroPowers = superHeroInfoData[0];
+    const pwrs = Object.keys(heroPowers).filter(key => key !== "hero_names");
+    res.json(pwrs);
+});
+
 app.listen(PORT, () => {
     console.log(`Running server hosted at http://localhost:${PORT}`);
 });
